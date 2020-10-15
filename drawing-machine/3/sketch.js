77b7c5557340fe07407448f853f3d281b0325d05
thisisalayna <56280884+thisisalayna@users.noreply.github.com>
2020-10-15 01:47:29 -0700

    .

--- drawing-machine/3/sketch.js
+++ drawing-machine/3/sketch.js
@@ -1,7 +1,25 @@
 function setup() {
   createCanvas(1000, 600);
+    background(200);
+
+
 }
 
 function draw() {
-  background(220);
+
+if (mouseIsPressed){
+  stroke(0,0,0, mouseX*(1/6));
+  strokeWeight(10);
+  line(width - mouseX, height - mouseY, width - pmouseX,height - pmouseY);
+  line(mouseX, mouseY, pmouseX, pmouseY)
+}
+
+}
+
+function keyTyped(){
+
+if (key =='p'|| key == 'P'){
+  saveCanvas('drawingmachine', 'png');
+}
+  return false;
 }
