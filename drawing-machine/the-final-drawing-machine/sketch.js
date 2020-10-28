7b3e264f2724b7867595be879ab8f000d779f388
thisisalayna <56280884+thisisalayna@users.noreply.github.com>
2020-10-27 19:59:28 -0700

    .

---
+++ drawing-machine/the-final-drawing-machine/sketch.js
@@ -0,0 +1,59 @@
+let array = [];
+let noiseOffset = 0.0;
+let strokeWidth = 5;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noFill();
+  background(0, 157, 220);
+
+}
+
+function draw() {
+  //background(0, 157, 220);
+  strokeWeight(strokeWidth);
+  noiseOffset += 0.05;
+  strokeWidth = noise(noiseOffset) * 50;
+
+  if (mouseIsPressed) {
+    stroke(map(mouseX, 0, 600, 0, 255, true));
+    strokeWeight(strokeWidth);
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY)
+    array.push([mouseX, mouseY]);
+
+
+
+    stroke(map(mouseX, 0, 600, 0, 255, true));
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY)
+
+  }
+}
+
+function keyTyped() {
+
+  if (key == 's' || key == 'S') {
+
+    saveCanvas('drawing', 'png');
+  } else if (key === 'c') {
+    //  clear();
+    background(0, 157, 220);
+  } else if (key === '1') {
+
+
+  } else if (key === '2') {
+
+
+  } else if (key === '3') {
+
+
+  } else if (key === '4'){
+
+
+  } else if (key === '5'){
+
+
+  }
+  return false;
+}
