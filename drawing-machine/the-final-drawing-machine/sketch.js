c81e233d0ad7f8b5059970080369ccf1de11dfd4
thisisalayna <56280884+thisisalayna@users.noreply.github.com>
2020-11-03 02:26:12 -0800

    update

--- drawing-machine/the-final-drawing-machine/sketch.js
+++ drawing-machine/the-final-drawing-machine/sketch.js
@@ -1,6 +1,6 @@
 let array = [];
 let noiseOffset = 0.0;
-let strokeWidth = 5;
+//let strokeWidth = 5;
 let choice = '1'
 
 function setup() {
@@ -10,11 +10,14 @@ function setup() {
 }
 
 function draw() {
-
+strokeWeight(10);
+stroke('white');
 cursor('grab');
+
   if (keyIsPressed) {
     choice = key;
     keyTyped(choice);
+
   }
   if (mouseIsPressed) {
     keyTyped(choice);
@@ -37,8 +40,6 @@ function keyTyped(keyPress) {
   } else if (keyPress === 'h' || keyPress === 'H') {
     myFunction();
   } else if (keyPress === '1') {
-    strokeWeight(10);
-    stroke(0);
     line(mouseX, mouseY, pmouseX, pmouseY);
 
   } else if (keyPress === '2') {
@@ -46,7 +47,11 @@ function keyTyped(keyPress) {
     line(mouseX, mouseY, pmouseX, pmouseY);
 
   } else if (keyPress === '3') {
-    textColor = 'black';
+
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    line(mouseX + 100, mouseY + 100, pmouseX + 100, pmouseY + 100);
+    line(width - mouseX + 100, height - mouseY + 100, width - pmouseX + 100, height - pmouseY + 100);
 
   } else if (keyPress === '4') {
 
