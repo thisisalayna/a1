b740edbc747a9bc19cb32a5cf091e4f7bab6db6f
thisisalayna <56280884+thisisalayna@users.noreply.github.com>
2020-11-03 15:28:50 -0800

    update

--- drawing-machine/the-final-drawing-machine/sketch.js
+++ drawing-machine/the-final-drawing-machine/sketch.js
@@ -1,29 +1,43 @@
 let array = [];
 let noiseOffset = 0.0;
-//let strokeWidth = 5;
-let choice = '1'
+let g;
+let colorPicker;
+let choice = '1';
+let button;
+let mousePressed;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0, 157, 220);
 
+g = createGraphics(200, 200);
+colorPicker = createColorPicker('#ffffff');
+colorPicker.size(50, 50);
+//g.position(20, 20);
 }
 
 function draw() {
 strokeWeight(10);
-stroke('white');
+stroke(colorPicker.color());
 cursor('grab');
+
+
+g.background(colorPicker.color());
+g.noStroke();
+g.ellipse(g.width / 2 , g.height / 2, 10, 10);
 
   if (keyIsPressed) {
     choice = key;
     keyTyped(choice);
 
   }
+
   if (mouseIsPressed) {
     keyTyped(choice);
     cursor('grabbing');
+}
 
-  }
   array.push([mouseX, mouseY]);
 
   }
@@ -37,8 +51,7 @@ function keyTyped(keyPress) {
   } else if (key === 'c') {
     //  clear();
     background(0, 157, 220);
-  } else if (keyPress === 'h' || keyPress === 'H') {
-    myFunction();
+
   } else if (keyPress === '1') {
     line(mouseX, mouseY, pmouseX, pmouseY);
 
@@ -54,12 +67,28 @@ function keyTyped(keyPress) {
     line(width - mouseX + 100, height - mouseY + 100, width - pmouseX + 100, height - pmouseY + 100);
 
   } else if (keyPress === '4') {
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    line(mouseX + 100, mouseY + 100, pmouseX + 100, pmouseY + 100);
+    line(width - mouseX + 100, height - mouseY + 100, width - pmouseX + 100, height - pmouseY + 100);
+    line(mouseX + 300, mouseY + 300, pmouseX + 300, pmouseY + 300);
+    line(width - mouseX + 300, height - mouseY + 300, width - pmouseX + 300, height - pmouseY + 300);
 
   } else if (keyPress === '5') {
-
+    strokeWeight(500);
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
   return false;
 }
+
+// function clouds(){
+//   noStroke();
+// ellipse(100, 100, 50);
+// ellipse(140, 110, 70, 50);
+// ellipse(190, 110, 50);
+// ellipse(150, 90, 80, 50);
+//
+// }
 
 function myFunction() {
   var x = document.getElementById("myDIV");
