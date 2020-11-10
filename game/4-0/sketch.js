a094fe6b8c3fd2e0e69115a3946d876f9786d293
thisisalayna <56280884+thisisalayna@users.noreply.github.com>
2020-11-10 02:04:20 -0800

    .

--- game/4-0/sketch.js
+++ game/4-0/sketch.js
@@ -1,11 +1,35 @@
-function setup() {
-  createCanvas(500, 500);
-  background(200);
+'use strict'
 
+let state ='title';
+let cnv;
+
+function setup() {
+  cnv = createCanvas(600, 600);
+  cnv.mouseClicked(function(){
+    console.log('canvas is CLICKED!');
+  });
 }
 
 
 function draw() {
+  background(200);
 
+  if (state === 'title') {
+    title();
+  } else if (state === 'level 1') {
+    level1();
+  }
 
+}
+
+function title() {
+  textSize(100);
+  stroke(255);
+  strokeWeight(3);
+  fill('purple');
+  text('This GAME!', 100, 100);
+}
+
+function level1() {
+  background(50, 50, 200);
 }
