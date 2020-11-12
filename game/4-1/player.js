a56385ddd04780126f2f8efc9035a49b28c6e0c9
thisisalayna <56280884+thisisalayna@users.noreply.github.com>
2020-11-12 12:39:13 -0800

    .

--- game/4-1/player.js
+++ game/4-1/player.js
@@ -1,12 +1,36 @@
 class Player {
-  constructor(){
+  constructor() {
     this.r = 60;
     this.x = w / 2;
     this.y = h - this.r;
+    this.direction = 'still';
   }
 
-display(){
+  display() {
     rect(this.x, this.y, this.r, this.r);
-}
+  }
+
+  move() {
+
+    switch (this.direction) {
+      case 'still':
+        // be still
+        break;
+      case 'up':
+        // decrease y
+        break;
+      case 'down':
+        // increase y
+        break;
+      case 'right':
+        //increase x
+        break;
+      case 'left':
+        //decrease x
+        break;
+
+
+    }
+  }
 
 }
