99ab120653c29ccdcce5306aff6102db33588f8d
thisisalayna <56280884+thisisalayna@users.noreply.github.com>
2020-11-19 14:33:42 -0800

    broken but updated

--- misc/giphy/sketch.js
+++ misc/giphy/sketch.js
@@ -1,17 +1,25 @@
 'use strict'
-var api = "http://api.giphy.com/v1/gifts/search?";
-var apiKey = "&api_key=dc6zaT0xFJmzC";
-var query = "&q=rainbow";
+//api.giphy.com
+
+// /v1/gifts/search
+
+
+let api = "http://api.giphy.com/v1/gifs/search?";
+let apiKey = "&api_key=dc6zaT0xFJmzC";
+let query = "&q=rainbow";
 
 function setup() {
   noCanvas();
-  var url = api + apiKey + query;
+  let url = api + apiKey + query;
   loadJSON(url, gotData);
 
 }
 
-function gotData (giphy) {
-createImg(giphy.data[0].images.original.url);
+function gotData(giphy) {
+println(giphy.data[i].images.original.url);
+  for (let i = 0; i < giphy.data.length; i++) {
+    createImg(giphy.data[i].images.original.url);
+ }
 }
 
 function draw() {
