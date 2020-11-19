23d84dd7b1c43335e74ad44c24bf1fb6031391fd
thisisalayna <56280884+thisisalayna@users.noreply.github.com>
2020-11-19 00:38:01 -0800

    .

--- misc/giphy/sketch.js
+++ misc/giphy/sketch.js
@@ -1,9 +1,17 @@
 'use strict'
-
+var api = "http://api.giphy.com/v1/gifts/search?";
+var apiKey = "&api_key=dc6zaT0xFJmzC";
+var query = "&q=rainbow";
 
 function setup() {
-  createCanvas(700, 700);
+  noCanvas();
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
 
+}
+
+function gotData (giphy) {
+createImg(giphy.data[0].images.original.url);
 }
 
 function draw() {
